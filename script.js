8f70f8e7d5014814d777e58f4c180b9b3a972218
Victor Henrique <123006884+victorhnbas@users.noreply.github.com>
2023-06-07 23:24:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function botaoEnter(){
+    $("#buttonBoasvindas").css("background-color","white")
+    $("#buttonBoasvindas").css("color","#60ac9c")
+    $("#buttonBoasvindas").css("cursor","pointer")
+    $("#buttonBoasvindas").css("transition","0.3s")
+}
+function botaoSair(){
+    $("#buttonBoasvindas").css("background-color","transparent")
+    $("#buttonBoasvindas").css("color","white")
+    $("#buttonBoasvindas").css("transition","0.3s")
+}
+
+function botaoCadastroEntrar(){
+    $("#buttonCriar").css("background-color","#3c796c")
+    $("#buttonCriar").css("cursor","pointer")
+    $("#buttonCriar").css("cursor","0.3s")
+}
+function botaoCadastroSair(){
+    $("#buttonCriar").css("background-color","#60ac9c")
+    $("#buttonCriar").css("cursor","pointer")
+    $("#buttonCriar").css("cursor","0.3s")
+}
